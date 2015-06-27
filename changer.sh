6a299bdebae85a9385ebcf5ce947f85be5eaafbd
Chinmaya Rath <crath@apigee.com>
2015-06-27 10:56:46 +0530

    Added some change in  dev

--- changer.sh
+++ changer.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
-echo " change " >> onefile.txt
+echo " change in dev " >> onefile.txt
 
 git add .
-git commit -m "Added some change"
-git push
+git commit -m "Added some change in  dev "
+
+git checkout master
+
+echo " change in master " >> onefile.txt
+
+git add .
+git commit -m "Added some change in master"
+
+git push --all
