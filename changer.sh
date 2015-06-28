5910c2a69b92117d96e476e0d8bc02db3036cad5
Chinmaya Rath <crath@apigee.com>
2015-06-28 08:41:31 +0530

    Added some change in  dev \n [skipci]

--- changer.sh
+++ changer.sh
@@ -5,13 +5,6 @@ git checkout dev
 echo " change in dev " >> onefile.txt
 
 git add .
-git commit -m "Added some change in  dev "
-
-git checkout master
-
-echo " change in master " >> onefile.txt
-
-git add .
-git commit -m "Added some change in master"
+git commit -m "Added some change in  dev \n [skipci]"
 
 git push --all
