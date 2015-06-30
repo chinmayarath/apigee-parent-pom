d0b61d5b0cfbbd79dba319bc941d5ab9417a9f7d
Chinmaya Rath <crath@apigee.com>
2015-06-30 10:30:48 +0530

    Added some change in  dev [alL:test]

--- changer.sh
+++ changer.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
 
-git checkout dev
+git checkout master
 
 echo " change in dev " >> onefile.txt
 
 git add .
-git commit -m "Added some change in  dev "
-
-git checkout master
-
-echo " change in master " >> onefile.txt
-
-git add .
-git commit -m "Added some change in master"
+git commit -m "Added some change in  dev [alL:test]"
 
 git push --all
