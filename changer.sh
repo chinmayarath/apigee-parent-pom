bdaa39862fb241a3f966dc1c9ac7416f7ab11866
Chinmaya Rath <crath@apigee.com>
2015-06-30 11:25:14 +0530

    Added some change in  dev [all:test]

--- changer.sh
+++ changer.sh
@@ -5,6 +5,6 @@ git checkout dev
 echo " change in dev " >> onefile.txt
 
 git add .
-git commit -m "Added some change in  dev [dummy:test]"
+git commit -m "Added some change in  dev [all:test]"
 
 git push --all
