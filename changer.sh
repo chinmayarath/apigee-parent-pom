13bf5e7963956bbff424374a44d67fcd6ddeb9f2
Chinmaya Rath <crath@apigee.com>
2015-06-30 12:26:49 +0530

    Added some change in  dev [  skip_ci  ]

--- changer.sh
+++ changer.sh
@@ -5,6 +5,6 @@ git checkout dev
 echo " change in dev " >> onefile.txt
 
 git add .
-git commit -m "Added some change in  dev [  dummy2   :  test  ]"
+git commit -m "Added some change in  dev [  skip_ci  ]"
 
 git push --all
