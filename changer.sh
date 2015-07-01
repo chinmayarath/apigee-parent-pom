ebdae3196fb12bf068d70f67988cbafab1e70895
Chinmaya Rath <crath@apigee.com>
2015-07-01 12:21:12 +0530

    Added some change in  dev [ erert :  ]

--- changer.sh
+++ changer.sh
@@ -5,6 +5,6 @@ git checkout dev
 echo " change in dev " >> onefile.txt
 
 git add .
-git commit -m "Added some change in  dev"
+git commit -m "Added some change in  dev [ erert :  ] "
 
 git push --all
